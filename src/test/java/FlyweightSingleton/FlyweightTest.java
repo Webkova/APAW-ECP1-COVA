42fd056349f4672130c73bdf37cb512538c664ef
Cova <Cova@DESKTOP-4E120RL>
2017-10-05 20:03:31 +0200

    Comments deleted

--- src/test/java/FlyweightSingleton/FlyweightTest.java
+++ src/test/java/FlyweightSingleton/FlyweightTest.java
@@ -1,15 +1,20 @@
 package FlyweightSingleton;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 
 import org.junit.Test;
 
+import FlyweightSingleton.*;
+import movie_original.Director;
+
+
+
 
 public class FlyweightTest {
     Factory factory;
-
-    
+        
     @Test
     public void testIsSingleton() {
         assertSame(Factory.getFactory(), Factory.getFactory());
@@ -20,5 +25,21 @@ public class FlyweightTest {
         assertNotNull(Factory.getFactory());
     }
     
+    
+//    @Test
+//    public void testGet() {
+//        Director director = new Director();
+//        director.setName("Denis Villeneuve");
+//        MovieConcrete movie = new MovieConcrete(8L);
+//        System.out.println(factory.getFactory().get(8L));
+////        assertEquals(8L, factory.get(8L).getId());
+//    }
+//
+//    @Test
+//    public void testSameObject() {
+//        assertSame(factory.get((long) 8), factory.get((long) 8));
+//    }
+
+
 
 }
