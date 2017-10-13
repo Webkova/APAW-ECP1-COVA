575fc7951b656fb24430e6ace87e8393e9b149d4
Cova <Cova@DESKTOP-4E120RL>
2017-10-13 13:17:07 +0200

    Prepare release

--- src/main/java/builder/MovieBuilder.java
+++ src/main/java/builder/MovieBuilder.java
@@ -7,45 +7,43 @@ import movie_original.Movie;
 public class MovieBuilder {
 
     private Movie movie;
+
     private Director director;
-   
-    
+
     public MovieBuilder id(long id) {
         this.movie = new Movie(id);
         return this;
     }
-    
+
     public MovieBuilder title(String title) {
         this.movie.setTitle(title);
         return this;
     }
-    
+
     public MovieBuilder debut(int debut) {
         this.movie.setDebut(debut);
         return this;
     }
-    
-  
-    public MovieBuilder DirectorName(String directorName){
+
+    public MovieBuilder DirectorName(String directorName) {
         this.director = new Director();
         this.movie.setDirector(director);
         this.movie.setDirectorName(directorName);
         return this;
     }
-    
-    
-    public MovieBuilder DirectorLevel(int directorLevel){
+
+    public MovieBuilder DirectorLevel(int directorLevel) {
         this.movie.setDirectorLevel(directorLevel);
         return this;
     }
-    
-   public MovieBuilder Genre(Genre gen){
-       this.movie.setGenre(gen);
-       return this;
-   }
-   
-   public Movie build() {
-       return this.movie;
-   }
-  
+
+    public MovieBuilder Genre(Genre gen) {
+        this.movie.setGenre(gen);
+        return this;
+    }
+
+    public Movie build() {
+        return this.movie;
+    }
+
 }
